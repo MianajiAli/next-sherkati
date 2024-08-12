841342906df924ea9f5f0037a10f8b7484e7efa5
MianajiAli <87234097+MianajiAli@users.noreply.github.com>
2024-08-12 13:11:53 +0330

    Add : animation to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,25 @@
 import Link from "next/link";
 import Product from "./TestNavbar";
+
 const Navbar = () => {
 	return (
 		<nav
-
-			className="w-full h-20 bg-back2 text-secondary flex flex-row justify-between items-center px-5 md:px-[5%] lg:px-[12%]"
+			className="w-full h-20 bg-back2 text-secondary flex flex-row justify-between items-center px-5 md:px-[3%] lg:px-[8%]"
 		>
 			<div className="relative flex flex-row justify-center items-center gap-1 h-18">
 				<img src="/images/logo.png" className="w-36" alt="" />
 			</div>
 
 			<div className="flex flex-row justify-center items-center gap-7">
-
-				<ul className="md:flex gap-5 hidden">
+				<ul className="lg:flex gap-5 xl:gap-10 hidden">
 					<li>
 						<Link href="/">
-							<span className="bg-primary text-back2 px-3 py-2 rounded cursor-pointer">صفحه اصلی</span>
+							<span className="bg-primary text-back2 px-3 py-2 rounded cursor-pointer">Home</span>
 						</Link>
 					</li>
 					<li>
 						<Link href="/">
-							<span className="px-3 py-2 rounded cursor-pointer">مقالات</span>
+							<span className="px-3 py-2 rounded cursor-pointer">Articles</span>
 						</Link>
 					</li>
 					<li>
@@ -28,20 +27,19 @@ const Navbar = () => {
 					</li>
 					<li>
 						<Link href="/">
-							<span className="px-3 py-2 rounded cursor-pointer">درباره ما</span>
+							<span className="px-3 py-2 rounded cursor-pointer">About Us</span>
 						</Link>
 					</li>
 					<li>
 						<Link href="/">
-							<span className="px-3 py-2 rounded cursor-pointer">تماس با ما</span>
+							<span className="px-3 py-2 rounded cursor-pointer">Contact Us</span>
 						</Link>
 					</li>
 				</ul>
 			</div>
 
-
 			<button className="py-2 px-6 rounded-md bg-success1 text-success2">
-				تماس با ما :  912123-021
+				Contact Us:  912123-021
 			</button>
 		</nav>
 	);
--- components/TestNavbar.tsx
+++ components/TestNavbar.tsx
@@ -1,15 +1,24 @@
-import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
+import { Popover, PopoverButton, PopoverPanel, Transition } from '@headlessui/react'
 
 export default function Product() {
     return (
         <Popover className="relative ">
             <PopoverButton>محصولات</PopoverButton>
-            <PopoverPanel anchor="bottom" className="flex flex-col bg-secondary text-secondary m-0 focus:outline-none border-0">
-                <a href="/analytics" className='py-3 px-5'>Analytics</a>
-                <a href="/engagement" className='py-3 px-5'>Engagement</a>
-                <a href="/security" className='py-3 px-5'>Security</a>
-                <a href="/integrations" className='py-3 px-5'>Integrations</a>
-            </PopoverPanel>
+            <Transition
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+            >
+                <PopoverPanel anchor="bottom" className="flex flex-col bg-secondary text-back2 rounded-md font-bold m-0 focus:outline-none border-0">
+                    <a href="/analytics" className='py-3 px-5'>Analytics</a>
+                    <a href="/engagement" className='py-3 px-5'>Engagement</a>
+                    <a href="/security" className='py-3 px-5'>Security</a>
+                    <a href="/integrations" className='py-3 px-5'>Integrations</a>
+                </PopoverPanel>
+            </Transition>
         </Popover>
     )
 }
