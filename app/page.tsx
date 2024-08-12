cbe496aaaf87e6d257dc7c195d57419a1e4e4035
MianajiAli <87234097+MianajiAli@users.noreply.github.com>
2024-08-12 10:45:25 +0330

    Add headless ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Aboutus from "@components/Aboutus";
 import Contact from "@components/Contact";
 import Hero1 from "@components/Hero1";
+import TestNavbar from "@components/TestNavbar";
+
 
 export default function Page() {
 	return (
 		<div>
+			<TestNavbar />
 			<Hero1></Hero1>
 			<Aboutus></Aboutus>
-			<Contact/>
+			<Contact />
 		</div>
 	);
 }
