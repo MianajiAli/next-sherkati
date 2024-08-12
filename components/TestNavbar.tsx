b7e0df374f1325903f3c67a557516083ab786101
MianajiAli <87234097+MianajiAli@users.noreply.github.com>
2024-08-12 12:22:03 +0330

    Fix: Button's outline

--- components/TestNavbar.tsx
+++ components/TestNavbar.tsx
@@ -4,7 +4,7 @@ export default function Product() {
     return (
         <Popover className="relative ">
             <PopoverButton>محصولات</PopoverButton>
-            <PopoverPanel anchor="bottom" className="flex flex-col bg-slate-200 margin: 0px;">
+            <PopoverPanel anchor="bottom" className="flex flex-col bg-slate-200 m-0 focus:outline-none border-0">
                 <a href="/analytics">Analytics</a>
                 <a href="/engagement">Engagement</a>
                 <a href="/security">Security</a>
